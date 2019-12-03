eaf31e10d2c21e5dc95b437ceb200552569d9ff8
likuma <53406754+likuma@users.noreply.github.com>
2019-12-03 21:35:22 +0100

    Update jenkins_create_ec2.tf

--- jenkins_create_ec2.tf
+++ jenkins_create_ec2.tf
@@ -18,18 +18,3 @@ resource "aws_instance" "http_server_with_ssh_icmp_from_jenkins" {
                 chkconfig httpd on
   EOF
 }
-
-#For testing creation
-resource "aws_instance" "just_for_test" {
-  ami           = "ami-0badcc5b522737046"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "ust_for_test"
-  }
-  subnet_id                   = data.aws_subnet.subnet_for_http_servers_attrs.id
-  vpc_security_group_ids      = [data.aws_security_group.sec_group_for_http_and_ssh_icmp_attrs.id]
-  associate_public_ip_address = true
-  key_name                    = "key_com_http"
-  
-}
-© 2019 GitHub, Inc.
