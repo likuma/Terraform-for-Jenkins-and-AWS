fb0640bba864afbba04c0f124af41812bbc742ee
likuma <53406754+likuma@users.noreply.github.com>
2019-12-06 10:25:24 +0100

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 #create sbunet for management servers like jenkins or pgadmim
 resource "aws_subnet" "subnet_for_mgmnt_servers" {
   vpc_id     = data.aws_vpc.default_vpc_attrs.id
-  cidr_block = var.CIDR_BLOCK_NON_PROD
+  cidr_block = var.CIDR_BLOCK_MGMNT
   tags = {
     Name = "subnet_for_mgmnt_servers"
   }
